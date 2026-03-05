a2e1fffd45693ded279577b7c3d1c6f6317d51c0
FajarK-Putra <225520211003@metamedia.ac.id>
2026-03-05 15:05:40 +0700

    fix: force dynamic rendering for news to prevent static cache of empty data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import HeroSection from '../components/templates/HeroSection'
 import NewsSection from '../components/templates/NewsSection'
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <>
